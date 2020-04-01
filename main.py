de53f947335e015e47883d23a2439a512dfabc65
JungChaeMoon <jungchaemoon@gmail.com>
2020-04-02 00:22:34 +0900

    Added objective function and plotting the energy value

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ x_data = np.random.normal(loc=0.0, scale=2.0, size=(100, 1)) #input_data  평균
 y_hat = [(x * 2) for x in x_data] # a = 2, b = 0 선정한후 y_hat 데이터 생성 type(np,darray)
 y_data = [y + np.random.normal(loc=0.0, scale=2.0) for y in y_hat] #평균 0 표준편차 2인 난수생성으로 y_hat 속성값에 더하기
 plt.scatter(x=x_data, y=y_data, edgecolors='black') #분산된 x_data, y_data 더하기
-plt.plot(x_data, y_hat, color='blue') # y_hat = 2x linear graph 그리기
+
+
 def draw_model_parameters(theta_zero_value, theta_one_value, cnt):
     plt.plot([i for i in range(0, cnt)], theta_zero_value, color='blue')
     plt.plot([i for i in range(0, cnt)], theta_one_value, color='red')
     plt.show()
+
+
+def objective_function(x_data, y_data, theta_one, theta_zero):
+
+    m = 100
+    theta_zero = theta_zero
+    theta_one = theta_one
+    error = 0
+
+    for i in range(0, m):
+        error += ((x_data[i][0] * theta_one + theta_zero) - y_data[i]) ** 2
+
+    error = error / (2 * m)
+
+    return error
 
 
 def finely_divided_function(x_data, y_data, theta_one, theta_zero):
@@ -72,6 +88,7 @@ def gradient_function(x_data, y_data):
     plt.plot(x_data, y_value_list, color='red')
     plt.scatter(x=x_data, y=y_data, color='blue')
     plt.show()
+    draw_energy_value(error_list, cnt)
 
 
 x_data = np.random.normal(loc=0.0, scale=8.0, size=(100, 1)) #input_data  평균 0 표준편차2 랜덤 x_data 생성
