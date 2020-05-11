63a06a71fafc7f969dc9eaa469db5813f59aca3e
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-11 20:09:10 +0900

    Plot the training data

---
+++ assignment7.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data    = np.genfromtxt("data-nonlinear.txt", delimiter=',')
+
+pointX  = data[:, 0]
+pointY  = data[:, 1]
+label   = data[:, 2]
+
+pointX0 = pointX[label == 0]
+pointY0 = pointY[label == 0]
+
+pointX1 = pointX[label == 1]
+pointY1 = pointY[label == 1]
+
+plt.figure()
+plt.scatter(pointX0, pointY0, c='b')
+plt.scatter(pointX1, pointY1, c='r')
+plt.tight_layout()
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
