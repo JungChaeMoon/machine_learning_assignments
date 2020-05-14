f2e3ac62564c0a7a261556385f6d76f587629756
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-14 20:00:27 +0900

    modified plot data

--- assignment7.py
+++ assignment7.py
@@ -1,11 +1,13 @@
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
+from math import e, log
 
-data    = np.genfromtxt("data-nonlinear.txt", delimiter=',')
+data = np.genfromtxt("/content/sample_data/data-nonlinear.txt", delimiter=',')
 
-pointX  = data[:, 0]
-pointY  = data[:, 1]
-label   = data[:, 2]
+pointX = data[:, 0]
+pointY = data[:, 1]
+label = data[:, 2]
 
 pointX0 = pointX[label == 0]
 pointY0 = pointY[label == 0]
@@ -13,9 +15,12 @@ pointY0 = pointY[label == 0]
 pointX1 = pointX[label == 1]
 pointY1 = pointY[label == 1]
 
-plt.figure()
-plt.scatter(pointX0, pointY0, c='b')
-plt.scatter(pointX1, pointY1, c='r')
-plt.tight_layout()
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+data_xy_val = []
+
+for t in range(len(pointX)):
+  tmp = []
+  for i in range(10):
+    for j in range(10):
+      tmp.append(pointX[t] ** i * pointY[t] ** j)
+  data_xy_val.append(tmp)
+
