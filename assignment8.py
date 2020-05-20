914ddce5f0c22bb0471c48f643d81764876baccf
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-21 02:29:03 +0900

    add img avg pixel

--- assignment8.py
+++ assignment8.py
@@ -10,3 +10,18 @@ for i in range(784):
 
 mnist = pd.read_csv("mnist_test.csv", names = col_names)
 
+mnist = np.array(mnist)
+digit_list = []
+for i in range(10):
+  digit = mnist[mnist[:,0] == i]
+  digit = np.array(digit)
+  digit_list.append(digit[:,1:])
+
+avg_img = []
+for digit_info in digit_list:
+  avg_digit = []
+  for i in range(len(digit_info.T)):
+    val = np.sum(digit_info[:,i])
+    avg_digit.append(val/len(digit_info))
+  avg_img.append(avg_digit)
+
