7401d5cef5d12c071c5db7f12629331073325717
JungChaeMoon <jungchaemoon@gmail.com>
2020-06-04 23:38:43 +0900

    declare function

--- assignment10.py
+++ assignment10.py
@@ -66,3 +66,62 @@ one_hot_encoding = [
 one_hot_encoding = np.array(one_hot_encoding)
 train_image = np.empty((size_row * size_col, 1000), dtype=float)
 test_image = np.empty((size_row * size_col, 9000), dtype=float)
+
+for i in range(num_image):
+  if i>= 1000:
+    test_image[:, i-1000] = list_image[:, i]
+  else:
+    train_image[:, i] = list_image[:, i]
+
+training_label = []
+testing_label = []
+for j in range(num_image):
+  label_value = list_label[j]
+  if j<1000:
+    training_label.append(one_hot_encoding[label_value])
+  else:
+    testing_label.append(one_hot_encoding[label_value])
+
+training_label = np.array(training_label).T
+testing_label = np.array(testing_label).T
+
+def sigmoid(x):
+  return 1 / (1+np.exp(-x))
+
+def sigmoid_p(x):
+  return sigmoid(z) * (1 - sigmoid(z))
+
+u = np.random.normal(0,1, 196*784).reshape((196,784))
+v = np.random.normal(0,1, 49*196).reshape((49,196))
+w = np.random.normal(0,1, 10*49).reshape((10,49))
+
+b1 = np.zeros((196,1))
+b2 = np.zeros((49,1))
+b3 = np.zeros((10,1))
+# u = np.random.normal(0,1, 196*784).reshape((196,784))
+# v = np.random.normal(0,1, 49*196).reshape((49,196))
+# w = np.random.normal(0,1, 10*49).reshape((10,49))
+
+# b1 = np.zeros((196,1))
+# b2 = np.zeros((49,1))
+# b3 = np.zeros((10,1))
+
+# u_bias = np.ones((1000, 1))
+# v_bias = np.ones((1000, 1))
+# w_bias = np.ones((1000, 1))
+
+# test_x_bias = np.ones((9000, 1))
+# test_u_bias = np.ones((9000, 1))
+# test_v_bias = np.ones((9000, 1))
+# test_w_bias = np.ones((9000, 1))
+
+# new_u_bias = 0.0
+# new_v_bias = 0.0
+# new_w_bias = 0.0
+def accuracy(label, sig):
+  correct = 0
+  length = len(label[0])
+  for i in range(length):
+    if label[sig[:, i].argmax(), i] == 1:
+      correct += 1
+  return (correct/length) * 100
